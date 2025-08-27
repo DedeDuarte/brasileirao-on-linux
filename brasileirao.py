fb1098932cc7b76aecf6e5fa6c680a23df29b4d3
DedeDuarte <andredoernerd@gmail.com>
2025-08-27 19:38:13 -0300

    Updating table system with Rich

--- brasileirao.py
+++ brasileirao.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-import requests
-from datetime import datetime, timedelta
 import os
-import json
-from dotenv import load_dotenv
 import sys
+import json
+import requests
+
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+
+from rich import box
+from rich.table import Table
+from rich.console import Console
 
 def getDataFromAPI(competition):
     load_dotenv()
@@ -78,38 +83,103 @@ def printTable(data):
     if data is None:
         raise Exception('No data avaliable')
 
-    print(f'======================================================================================\n'
-          f'|  # | '
-          f'pt '
-          f'Team          | '
-          f' G | '
-          f' W (wRate%) | '
-          f' D (dRate%) | '
-          f' L (lRate%) | '
-          f' GF  GA Dif |\n'
-          f'|====|==================|====|=============|=============|=============|=============|')
+    console = Console()
+
+    if '-s' in sys.argv:
+        table = Table(title='Tabela Brasileirão', box=box.SIMPLE)
+    else:
+        try:
+            table = Table(title='Tabela Brasileirão', box=box.ROUNDED2)
+        except Exception:
+            table = Table(title='Tabela Brasileirão', box=box.SIMPLE)
+
+    table.add_column('', justify='center', style='bold')
+    table.add_column('#', justify='right', style='bold')
+    table.add_column('pt', justify='right')
+    table.add_column('Team', justify='left')
+    table.add_column('G', justify='right')
+    table.add_column('W', justify='right')
+    table.add_column('Win %', justify='right')
+    table.add_column('D', justify='right')
+    table.add_column('Drw %', justify='right')
+    table.add_column('L', justify='right')
+    table.add_column('Los %', justify='right')
+    table.add_column('GF', justify='right')
+    table.add_column('GA', justify='right')
+    table.add_column('Dif', justify='right')
 
     for p in data['standings'][0]['table']:
         position = p['position']
         points = p['points']
-        name = p["team"]["shortName"]
+        name = p['team']['shortName']
+
         playedGames =  p['playedGames']
         won = p['won']
         draw = p['draw']
         lost = p['lost']
+        winRate = won / playedGames
+        drawRate = draw / playedGames
+        lostRate = lost / playedGames
+
         goalsFor = p['goalsFor']
         goalsAgainst = p['goalsAgainst']
         goalDifference = p['goalDifference']
 
-        print(f'| {position:2} | '
-              f'{points:2} '
-              f'{name:13} | '
-              f'{playedGames:2} | '
-              f'{won :2} ({won  / playedGames * 100:5.1f}%) | '
-              f'{draw:2} ({draw / playedGames * 100:5.1f}%) | '
-              f'{lost:2} ({lost / playedGames * 100:5.1f}%) | '
-              f'{goalsFor:3} {goalsAgainst:3} {goalDifference:3} |')
-    print(f'======================================================================================')
+        if name == 'São Paulo': style = '#aa0000 bold'
+        else: style = ''
+
+        if position in range(1, 5):
+            division = f'[#007000]{"|"}[/#007000]'
+        elif position in range(5, 7):
+            division = f'[#dddd00]{"|"}[/#dddd00]'
+        elif position in range(7, 13):
+            division = f'[#00ffff]{"|"}[/#00ffff]'
+        elif position in range(17, 21):
+            division = f'[#ff0000]{"|"}[/#ff0000]'
+        else:
+            division = f'{"|"}'
+
+        table.add_row(*map(str, [division, position, points, name, playedGames, won, f'{winRate:.1%}', draw, f'{drawRate:.1%}',lost, f'{lostRate:.1%}',goalsFor, goalsAgainst, goalDifference]), style=style)
+
+
+
+
+    console.print(table)
+
+
+
+    # print(f'======================================================================================\n'
+    #       f'|  # | '
+    #       f'pt '
+    #       f'Team          | '
+    #       f' G | '
+    #       f' W (wRate%) | '
+    #       f' D (dRate%) | '
+    #       f' L (lRate%) | '
+    #       f' GF  GA Dif |\n'
+    #       f'|====|==================|====|=============|=============|=============|=============|')
+
+    # for p in data['standings'][0]['table']:
+    #     position = p['position']
+    #     points = p['points']
+    #     name = p["team"]["shortName"]
+    #     playedGames =  p['playedGames']
+    #     won = p['won']
+    #     draw = p['draw']
+    #     lost = p['lost']
+    #     goalsFor = p['goalsFor']
+    #     goalsAgainst = p['goalsAgainst']
+    #     goalDifference = p['goalDifference']
+
+    #     print(f'| {position:2} | '
+    #           f'{points:2} '
+    #           f'{name:13} | '
+    #           f'{playedGames:2} | '
+    #           f'{won :2} ({won  / playedGames * 100:5.1f}%) | '
+    #           f'{draw:2} ({draw / playedGames * 100:5.1f}%) | '
+    #           f'{lost:2} ({lost / playedGames * 100:5.1f}%) | '
+    #           f'{goalsFor:3} {goalsAgainst:3} {goalDifference:3} |')
+    # print(f'======================================================================================')
 
 if __name__ == '__main__':
     data = getData('bsa')
